954d005cd6df74d63d6a266712073f553d922986
tommychang97 <tommychang97@gmail.com>
2020-03-15 16:47:15 -0700

    added db

--- util/database.js
+++ util/database.js
@@ -1,12 +1,12 @@
 const Pool = require('pg').Pool;
 
 const pool = new Pool({
-    host:'localhost',
-    user:'postgres',
-    database:'artists',
-    password:'',
+    host:'ec2-34-206-252-187.compute-1.amazonaws.com',
+    user:'rtqycqhijwfqsp',
+    database:'dfehpecmt4ptdd',
+    password:'74160b72ffe634f65d1b8a5a97be174788c45b8fded5f20e38fc654033b77593',
     port: 5432,
-    ssl: false
+    ssl: true
     });
     
 module.exports = pool;
